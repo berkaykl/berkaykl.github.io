27205ff8aac860d15751ba295b6727e652fefbaa
Berkay <89808875+berkaykl@users.noreply.github.com>
2021-08-30 23:20:01 +0300

    berkaykl.github.io
    Personal Website

---
+++ scripts.js
@@ -0,0 +1,40 @@
+$(function() {
+  $('.social a').css('color','#8a8a8a');
+  $('.mail').css('color', '#8a8a8a');
+
+  $('.mail').mouseenter(function(){
+    $('.mail').css('color','white');
+  });
+  $('.mail').mouseleave(function(){
+    $('.mail').css('color','#8a8a8a');
+  });
+
+  $('.social a:first-child').mouseenter(function(){
+    $('.social a:first-child').css('color','white');
+  });
+  $('.social a:first-child').mouseleave(function(){
+    $('.social a:first-child').css('color','#8a8a8a');
+  });
+
+  $('.social a:nth-child(2)').mouseenter(function(){
+    $('.social a:nth-child(2)').css('color','white');
+  });
+  $('.social a:nth-child(2)').mouseleave(function(){
+    $('.social a:nth-child(2)').css('color','#8a8a8a');
+  });
+
+  $('.social a:nth-child(3)').mouseenter(function(){
+    $('.social a:nth-child(3)').css('color','white');
+  });
+  $('.social a:nth-child(3)').mouseleave(function(){
+    $('.social a:nth-child(3)').css('color','#8a8a8a');
+  });
+
+  $('.social a:nth-child(4)').mouseenter(function(){
+    $('.social a:nth-child(4)').css('color','white');
+  });
+  $('.social a:nth-child(4)').mouseleave(function(){
+    $('.social a:nth-child(4)').css('color','#8a8a8a');
+  });
+
+});
