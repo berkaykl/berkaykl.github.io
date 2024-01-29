28a1417bc8e5d71992b7daab7d335ae9cf5884ca
Berkay <89808875+berkaykl@users.noreply.github.com>
2024-01-29 17:53:16 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 //TYPED JS
-var typed = new Typed('#my-infos', {
+/*var typed = new Typed('#my-infos', {
     strings: ['Frontend Developer', 'Student', 'UI/UX Designer'],
     typeSpeed: 50,
     backSpeed:30,
     loop: true,
-});
+});*/
