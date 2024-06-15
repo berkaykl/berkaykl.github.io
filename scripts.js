a0c0167ea2441e98dfbc40cbff29df37416b7854
Berkay <89808875+berkaykl@users.noreply.github.com>
2024-06-15 14:18:46 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //TYPED JS
 var typed = new Typed('#top-section-name-features', {
-    strings: ['Web Developer', 'Mobile Application Developer', 'Database Control', 'Student', 'UI/UX Designer'],
+    strings: ['Web Developer', 'Mobile Application Developer', 'Database Controller', 'Student', 'UI/UX Designer'],
     typeSpeed: 50,
     backSpeed:30,
     loop: true,
